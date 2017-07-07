8992815b53f68684252bd643a02699fea1d38acd
Artur Kmera <artur.kmera@3code.pl>
2017-07-07 12:24:19 +0200

    Updates

--- src/packages/common/layouts/EmptyLayout.php
+++ src/packages/common/layouts/EmptyLayout.php
@@ -38,33 +38,7 @@ class EmptyLayout extends \Arrow\Models\AbstractLayout{
     }
 
     //todo zmienic na standard
-    public function printStandartMenu($addText, $editText, $obj, $objNameField = "name", $editProtection = false)
-    {
-        if ($obj) {
-            if ($objNameField)
-                $editText = $obj[$objNameField];
-            else
-                $editText = $obj->getFriendlyName();
-        }
-        $str = "";
-        //$str.= "<span>{$obj[$objNameField]}</span>";
-        $str .= '<h3><i class="icon-circle-arrow-right"></i>';
-        if ($obj)
-            $str .= $editText . " ";
-        else
-            $str .= $addText;
-        $str .= "</h3>";
-        $str .= '<div class="top-actions">';
-        if ($obj)
-            $str .= '<a class="save" href="#" ><i class=" icon-check"></i>Zatwierdź</a>';
 
-        if ($editProtection && $obj)
-            $str .= '<a class="edit-protection" href="#" ><i class="icon-ok-sign"></i>Kliknij aby edytować</a>';
-
-        $str .= '<a class="save exit" href="#" ><i class="icon-circle-arrow-right"></i> Zatwierdź i zamknij</a><a class="cancel" href="#"  ><i class="icon-signout"></i> Anuluj</a>';
-
-        return $str . '</div>';
-    }
 }
 
 ?>
