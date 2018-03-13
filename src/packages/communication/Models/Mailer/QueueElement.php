7644157af372d4dd5edf747aa567e8b3d3d2101a
Art Ble <artur.kmera@3code.pl>
2018-03-13 15:45:10 +0100

    Mailer Queue content moved to file

--- src/packages/communication/Models/Mailer/QueueElement.php
+++ src/packages/communication/Models/Mailer/QueueElement.php
@@ -3,19 +3,52 @@ namespace Arrow\Communication\Models\Mailer;
 
 
 use Arrow\ORM\ORM_Arrow_Communication_Models_Mailer_QueueElement;
+use Exception;
+use function date;
+use function file_exists;
 use function file_put_contents;
+use function mkdir;
 
 class QueueElement extends ORM_Arrow_Communication_Models_Mailer_QueueElement
 {
 
+    private $folder;
+
+    public function __construct($data = null, ?array $parameters = null)
+    {
+        parent::__construct($data, $parameters);
+        $this->folder = ARROW_DOCUMENTS_ROOT . "/data/mailerQueue/" . date("Y-m");
+
+    }
+
+
     public function setContent($content)
     {
-        file_put_contents(ARROW_DOCUMENTS_ROOT . "/data/mailerQueue/" . $this->_id() . ".txt", $content);
+
+        if (!file_exists($this->folder)) {
+            mkdir($this->folder);
+        }
+
+        file_put_contents($this->folder . "/" . $this->_id() . ".txt", $content);
     }
 
     public function getContent()
     {
-        file_get_contents(ARROW_DOCUMENTS_ROOT . "/data/mailerQueue/" . $this->_id() . ".txt");
+
+        $file = $this->folder . "/" . $this->_id() . ".txt";
+        if (file_exists($file)) {
+            return file_get_contents($this->folder . "/" . $this->_id() . ".txt");
+        } else {
+            throw new Exception("Content file dosn't exists [{$this->_id()}] ");
+        }
+    }
+
+    public function getAttachments()
+    {
+        if ($this->_attachments() == "") {
+            return [];
+        }
+        return explode(",,,", $this->_attachments());
     }
 
 
