7996ab0bd5ebdcf003741f3ff2d5b8c2352c6a81
Art Ble <artur.kmera@3code.pl>
2018-04-17 06:44:19 +0200

    Improvement log managment

--- src/packages/synchronization/views/synchronizationcontroller/log.component.tsx
+++ src/packages/synchronization/views/synchronizationcontroller/log.component.tsx
@@ -10,6 +10,7 @@ import Comm from "frontend/src/lib/Comm";
 import {LoadingIndicator} from "frontend/src/ctrl/LoadingIndicator";
 import Icon from "frontend/src/ctrl/Icon";
 import {Modal} from "frontend/src/ctrl/Overlays";
+import {BForm, BSelect, BSwitch, BText} from "../../../../../../../../node_modules_shared/frontend/src/layout/BootstrapForm";
 
 interface IProps extends IArrowViewComponentProps {
     options: any;
@@ -17,14 +18,13 @@ interface IProps extends IArrowViewComponentProps {
 
 export default class ArrowViewComponent extends React.Component<IProps, any> {
 
-
-    table: Table;
+    public table: Table;
 
     constructor(props) {
         super(props);
         this.state = {
             currSync: false,
-            currTextPresentation: false
+            currTextPresentation: false,
         };
 
     }
@@ -38,7 +38,7 @@ export default class ArrowViewComponent extends React.Component<IProps, any> {
         });
     }
 
-    handleOpen(what: any): any {
+    public handleOpen(what: any): any {
         window.open(this.props._basePath + this.props._baseURL + `/runSynch/${what.actionName}/1`);
     }
 
@@ -58,23 +58,76 @@ export default class ArrowViewComponent extends React.Component<IProps, any> {
                 {currSync && <Panel noPadding={true}><LoadingIndicator text={currSync.label}/></Panel>}
 
                 <Row md={[5, 7]} noGutters={false}>
-                    <Panel title={"Dostępne synchronizacje"} noPadding={true} icon={"Sync"}>
-                        <ul className={"tasks-list"}>
-                            {p.options.map((e) => <li key={e.actionName}>
-                                    <a onClick={() => this.handleSync(e)}>{e.label}</a>
-                                    {e.subTasks && <ul>
-                                        {e.subTasks.map((sub) => <li key={sub.actionName}>
-                                            <a>
-                                                <span onClick={() => this.handleSync(sub)}>{sub.label}</span>
-                                                <span onClick={() => this.handleOpen(sub)} style={{float: 'right'}}><Icon name={"OpenInNewWindow"}/> </span>
-                                            </a>
-                                        </li>)}
-                                    </ul>}
-                                </li>,
-                            )}
-                        </ul>
-                    </Panel>
+                    <div>
+                        <Panel title={"Dostępne synchronizacje"} noPadding={true} icon={"Sync"}>
+                            <ul className={"tasks-list"}>
+                                {p.options.map((e) => <li key={e.actionName}>
+                                        <a onClick={() => this.handleSync(e)}>{e.label}</a>
+                                        {e.subTasks && <ul>
+                                            {e.subTasks.map((sub) => <li key={sub.actionName}>
+                                                <a>
+                                                    <span onClick={() => this.handleSync(sub)}>{sub.label}</span>
+                                                    <span onClick={() => this.handleOpen(sub)} style={{float: "right"}}><Icon name={"OpenInNewWindow"}/> </span>
+                                                </a>
+                                            </li>)}
+                                        </ul>}
+                                    </li>,
+                                )}
+                            </ul>
+                        </Panel>
 
+                        <Panel title={"Konfiguracja uruchomienia"} noPadding={true} icon={"Sync"}>
+                            <BForm
+                            >
+                                {(form, data) => {
+                                    const type = data.type;
+                                    const repeatType = data.repeat_type;
+
+                                    return <div style={{padding: 10}}>
+                                        <pre>{JSON.stringify(data, null, 2)}</pre>
+                                        <Row noGutters={false}>
+                                            <BSwitch
+                                                label={"Kiedy wywołać zadanie"}
+                                                options={[
+                                                    {value: "time_in_day", label: "Konkretna pora dnia"},
+                                                    {value: "every_hour", label: "Każdej godziny"},
+                                                    {value: "every_x_minutes", label: "Co x minut"},
+                                                ]}
+                                                {...form("type")}
+                                            />
+                                        </Row>
+
+                                        <Row noGutters={false} md={[2, 2]}>
+                                            {type == "time_in_day" && <BText label={"Godzina"} {...form("hour_in_day")}/>}
+                                            {(type == "time_in_day" || type == "every_hour") && <BText label={"Minuta"} {...form("minutes_in_hour")}/>}
+                                        </Row>
+                                        {type == "every_x_minutes" && <Row noGutters={false} md={[2, 3]}>
+                                            <BText label={"Ilośc minut"} {...form("minutes")} />
+                                            <BText label={"Przesunięcie ( od północy )"} {...form("minutes_offset", 0)} />
+                                        </Row>}
+
+                                        <Row noGutters={false} md={[4, 2]}>
+                                            <BSelect
+                                                label={"Powtarzaj"}
+                                                options={[
+                                                    {value: "daily", label: "Codziennie"},
+                                                    {value: "weekly", label: "Tygodniowo"},
+                                                    {value: "monthly", label: "Co miesiąc"},
+                                                    {value: "last_day_of_month", label: "Ostatni dzień miesiąca"},
+                                                ]}
+                                                {...form("repeat_type", "daily")}
+                                            />
+                                        </Row>
+                                        {(repeatType == "weekly" || repeatType == "monthly") && <Row noGutters={false} md={[4, 2]}>
+                                            <BText label={"Dni"} help={"Podaj dni po przecinku"} {...form("days")} />
+                                        </Row>}
+
+                                    </div>;
+                                }}
+                            </BForm>
+
+                        </Panel>
+                    </div>
 
                     <Panel noPadding={true}>
                         {/*{currSync && <div>
@@ -94,7 +147,7 @@ export default class ArrowViewComponent extends React.Component<IProps, any> {
                                 Column.text("errors", "Błędy")
                                     .template((val) => {
                                         if (val.length > 0) {
-                                            return <a className={"sync-error-icon"}><Icon name={"Error"}/></a>
+                                            return <a className={"sync-error-icon"}><Icon name={"Error"}/></a>;
                                         }
                                     })
                                     .className("center")
@@ -105,7 +158,7 @@ export default class ArrowViewComponent extends React.Component<IProps, any> {
                                 Column.text("output", "Info")
                                     .template((val) => {
                                         if (val.length > 0) {
-                                            return <a className={"sync-info-icon"}><Icon name={"InfoSolid"}/></a>
+                                            return <a className={"sync-info-icon"}><Icon name={"InfoSolid"}/></a>;
                                         }
                                     })
                                     .className("center")
