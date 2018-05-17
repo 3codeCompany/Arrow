a6587309f84fa45ef74ca6a5b8b97639088343b6
adrian.sliwka <adrian.sliwka@3code.pl>
2018-05-17 15:49:34 +0200

    SearcherHelper

--- src/packages/common/Models/Helpers/SearcherHelper.php
+++ src/packages/common/Models/Helpers/SearcherHelper.php
@@ -8,6 +8,9 @@
 
 namespace Arrow\Common\Models\Helpers;
 
+use App\Models\Translations\CountryVersion;
+use App\Models\Translations\Helper;
+use Arrow\Access\Models\Auth;
 use Arrow\Common\Models\Wigets\Table\TableDataSource;
 use Arrow\Media\Models\MediaAPI;
 use Arrow\ORM\Persistent\Criteria;
@@ -16,9 +19,23 @@ use Arrow\Shop\Models\Persistent\Product;
 use Arrow\Shop\Models\Persistent\ProductVariant;
 use Arrow\Shop\Models\Persistent\Promotion;
 use Arrow\Shop\Models\Persistent\PromotionProduct;
+use Arrow\Translations\Models\Translations;
+
 
 class SearcherHelper
 {
+    private $user;
+    private $country = "pl";
+
+    public function __construct()
+    {
+        $this->user = Auth::getDefault()->getUser();
+        if ($this->user->isInGroup("Partnerzy sprzedaży")) {
+            $this->country = substr($this->user->_login(), -2);
+            Translations::setupLang($this->country);
+        }
+    }
+
     public function getProductsProposition($key, $typeWord)
     {
         if (strpos($typeWord, "-") !== false)
@@ -52,8 +69,14 @@ class SearcherHelper
             ];
         }
 
+        $translated = Helper::translateProductsName($returnArr);
+        foreach ($returnArr as $key => $value)
+        {
+            $returnArr[$key]["name"] = $translated[$key]["name"];
+        }
+
         return [
-            "items" => $returnArr
+            "items" => $returnArr,
         ];
     }
 }
