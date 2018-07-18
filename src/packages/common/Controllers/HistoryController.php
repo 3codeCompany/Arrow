72af9b3fe6b397054dd442c6bbf561a08bfa3b10
Artur Kmera <artur.kmera@3code.pl>
2018-07-18 15:36:28 +0200

    Added history controller

--- src/packages/common/Controllers/HistoryController.php
+++ src/packages/common/Controllers/HistoryController.php
@@ -8,8 +8,40 @@
 
 namespace Arrow\Common\Controllers;
 
+use App\Models\CRM\History;
+use Arrow\Access\Models\User;
+use Arrow\Common\Models\Helpers\TableListORMHelper;
+use Arrow\ORM\Persistent\Criteria;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HistoryController
+ * @package Arrow\Common\Controllers
+ * @Route("/history")
+ */
 class HistoryController
 {
+    /**
+     * @Route("/{model}/{key}")
+     */
+    public function getTableData($model, $key)
+    {
+        $helper = new TableListORMHelper();
+
+
+
+        $criteria = History::get()
+            ->_class($model)
+            ->_elementId($key)
+            ->_join(User::class, [History::F_USER_ID => "id"], "U", [User::F_LOGIN]);
+
+
+        $helper->addDefaultOrder("id", Criteria::O_DESC);
+
+
+        return $helper->getListData($criteria);
+
+
+    }
 
 }
