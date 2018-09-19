7662286b0b550288da5b82a59bd9514522622ebe
Artur Kmera <artur.kmera@3code.pl>
2018-09-19 16:47:47 +0200

    Socket logger

--- src/packages/utils/Models/Debug/SocketLogger.php
+++ src/packages/utils/Models/Debug/SocketLogger.php
@@ -11,5 +11,25 @@ namespace Arrow\Utils\Models\Debug;
 
 class SocketLogger
 {
+    private $address = "127.0.0.1";
+    private $port = "1337";
+
+    public function __construct()
+    {
+
+    }
+
+    public function log($msg)
+    {
+        $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_connect($sock, $this->address, $this->port);
+
+        if (!is_string($msg)) {
+            $msg = print_r($msg, 1);
+        }
+        socket_write($sock, $msg, strlen($msg));
+        socket_close($sock);
+    }
+
 
 }
