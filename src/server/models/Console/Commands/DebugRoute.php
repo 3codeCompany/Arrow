f835b2b0e041b8edd900afc3a4ac14c20ecd1de9
Artur Kmera <artur.kmera@3code.pl>
2018-10-30 10:58:35 +0100

    Fix debug route command + add uid to media elements

--- src/server/models/Console/Commands/DebugRoute.php
+++ src/server/models/Console/Commands/DebugRoute.php
@@ -9,11 +9,13 @@
 namespace Arrow\Models\Commands;
 
 use Arrow\Kernel;
+use Arrow\Models\AnnotationRouteManager;
 use Arrow\Router;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class DebugRoute extends Command
 {
