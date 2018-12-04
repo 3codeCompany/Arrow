5193c83386bfed8d212ed9c0ea2e66661f4e1320
Artur Kmera <artur.kmera@3code.pl>
2018-12-04 16:01:45 +0100

    Fixed group add

--- src/packages/access/views/groups/edit.component.tsx
+++ src/packages/access/views/groups/edit.component.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 
-import { Navbar } from 'frontend/lib/Navbar';
-import {BForm, BText, BTextarea} from 'frontend/lib/BForm';
-import { Panel } from 'frontend/lib/Panel';
-import {Row} from 'frontend/lib/Row';
-import {Icon} from "frontend/lib/Icon"
+import { Navbar } from "frontend/lib/Navbar";
+import { BForm, BText, BTextarea } from "frontend/lib/BForm";
+import { Panel } from "frontend/lib/Panel";
+import { Row } from "frontend/lib/Row";
+import { Icon } from "frontend/lib/Icon";
 
-export default class  extends React.Component<any, any> {
+export default class extends React.Component<any, any> {
     constructor(props) {
         super(props);
         this.state = {
-            formData: {...props.group},
-            response: {}
+            formData: { ...props.group },
+            response: {},
         };
     }
 
@@ -19,50 +19,59 @@ export default class  extends React.Component<any, any> {
         this.forceUpdate();
         let data = this.refs.form.getData();
 
-        this.setState({formData: data});
+        this.setState({ formData: data });
     }
 
     handleFormSuccess(e) {
         this.props._notification(`Zapisano ${e.form.getData().name}`);
     }
 
-
     render() {
         let data = this.state.formData || {};
         return (
-          <div>
-              <Navbar>
-                  <span>System</span>
-                  <a onClick={() => this.props._goto( this.props._baseURL + '/list')}>Grupy dostępu</a>
-                  <span>{this.props.group ? this.props.group.name : 'Dodaj'}</span>
-              </Navbar>
+            <div>
+                <Navbar>
+                    <span>System</span>
+                    <a onClick={() => this.props._goto(this.props._baseURL + "/list")}>Grupy dostępu</a>
+                    <span>{this.props.group ? this.props.group.name : "Dodaj"}</span>
+                </Navbar>
 
+                <div className="panel-body-margins">
+                    <BForm
+                        ref="form"
+                        data={data}
+                        namespace={"data"}
+                        action={this.props._baseURL + "/save"}
+                        onSuccess={this.handleFormSuccess.bind(this)}
+                        onChange={this.handleFormChange.bind(this)}
+                    >
+                        {(form) => (
+                            <Row md={[6]}>
+                                <Panel
+                                    title={"Formularz " + (this.props.group ? "edycji" : "dodania") + " grupy dostępu"}
+                                >
+                                    <BText label="Nazwa" {...form("name")} />
+                                    <BTextarea label="Opis" {...form("description")} />
 
-              <BForm
-                ref="form"
-                data={data}
-                namespace={'data'}
-                action={this.props._baseURL + '/save'}
-                onSuccess={this.handleFormSuccess.bind(this)}
-                onChange={this.handleFormChange.bind(this)}
-              >
-                  {(form) => <Row md={[6]}>
-                      <Panel title={'Formularz ' + (this.props.group ? 'edycji' : 'dodania') + ' grupy dostępu'}>
-                          <BText label="Nazwa" {...form('name')} />
-                          <BTextarea label="Opis" {...form('description')} />
-
-                          <div className="hr-line-dashed"></div>
-                          <a onClick={() => this.props._goto(this.props._baseURL + '/list')} className="btn btn-default pull-right"> Anuluj</a>
-                          <button type="submit" className="btn btn-primary pull-right "> Zapisz</button>
-                          <div className="clearfix"></div>
-
-
-                      </Panel>
-                  </Row>}
-              </BForm>
-
-
-          </div>
+                                    <div className="hr-line-dashed" />
+                                    <a
+                                        onClick={() => this.props._goto(this.props._baseURL + "/list")}
+                                        className="btn btn-default pull-right"
+                                    >
+                                        {" "}
+                                        Anuluj
+                                    </a>
+                                    <button type="submit" className="btn btn-primary pull-right ">
+                                        {" "}
+                                        Zapisz
+                                    </button>
+                                    <div className="clearfix" />
+                                </Panel>
+                            </Row>
+                        )}
+                    </BForm>
+                </div>
+            </div>
         );
     }
 }
--- src/packages/access/views/groups/list.component.tsx
+++ src/packages/access/views/groups/list.component.tsx
@@ -6,8 +6,9 @@ import { Column, Table } from "frontend/lib/Table";
 import { Panel } from "frontend/lib/Panel";
 import { Icon } from "frontend/lib/Icon";
 import { Row } from "frontend/lib/Row";
-import {Comm} from "frontend/lib/lib";
+import { Comm } from "frontend/lib/lib";
 import { confirmDialog } from "frontend/lib/ConfirmDialog";
+import { CommandBar } from "frontend/lib/CommandBar";
 
 export default class ArrowViewComponent extends React.Component<any, any> {
     public table: Table;
@@ -29,36 +30,38 @@ export default class ArrowViewComponent extends React.Component<any, any> {
     public render() {
         return (
             <div>
+                <CommandBar
+                    items={[
+                        {
+                            key: "f1",
+                            label: "Dodaj",
+                            icon: "Add",
+                            onClick: () => this.props._goto(this.props._baseURL + "/edit"),
+                        },
+                    ]}
+                />
+
                 <Navbar>
                     <span>System</span>
                     <span>Grupy dostępu</span>
                 </Navbar>
-                <Row>
-                    <Panel
-                        title="Lista grup dostępu systemu"
-                        toolbar={[
-                            <a key="f1" href={"#" + this.props._baseURL + "/edit"} className="btn btn-sm btn-primary">
-                                <i className="fa fa-plus" />
-                                Dodaj
-                            </a>,
+
+                <div className="panel-body-margins">
+                    <Table
+                        remoteURL={this.props._baseURL + "/getData"}
+                        ref={(table) => (this.table = table)}
+                        columns={[
+                            Column.id("id", "Id"),
+                            Column.text("name", "Nazwa"),
+                            Column.template("Zobacz", () => <Icon name={"Edit"} />)
+                                .onClick((row) => this.props._goto(this.props._baseURL + "/edit", { key: row.id }))
+                                .className("center darkgreen"),
+                            Column.template("", () => <Icon name={"Delete"} />)
+                                .onClick(this.handleDelete.bind(this))
+                                .className("center darkred"),
                         ]}
-                    >
-                        <Table
-                            remoteURL={this.props._baseURL + "/getData"}
-                            ref={(table) => (this.table = table)}
-                            columns={[
-                                Column.id("id", "Id"),
-                                Column.text("name", "Nazwa"),
-                                Column.template("Zobacz", () => <Icon name={"Edit"} />)
-                                    .onClick((row) => this.props._goto(this.props._baseURL + "/edit", { key: row.id }))
-                                    .className("center darkgreen"),
-                                Column.template("", () => <Icon name={"Delete"} />)
-                                    .onClick(this.handleDelete.bind(this))
-                                    .className("center darkred"),
-                            ]}
-                        />
-                    </Panel>
-                </Row>
+                    />
+                </div>
             </div>
         );
     }
