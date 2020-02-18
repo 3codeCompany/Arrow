bf9eb9f5aed1196d2da00b8fbab68f66b1ba3da0
Pawel Witkowicz <pawel.witkowicz@3code.pl>
2020-02-18 11:39:18 +0100

    donwload src/packages/cms/

---
+++ src/packages/cms/views/bannercontroller/index.component.tsx
@@ -0,0 +1,234 @@
+import * as React from "react";
+import Navbar from "frontend/src/ctrl/Navbar";
+import {Column, Table} from "frontend/src/ctrl/Table";
+
+import {confirm, Modal} from "frontend/src/ctrl/Overlays";
+import {BForm, BSelect, BSwitch, BText, BTextarea, BWysiwig} from "frontend/src/layout/BootstrapForm";
+import Comm from "frontend/src/lib/Comm";
+import {Row} from "frontend/src/layout/BootstrapLayout";
+
+import {IArrowViewComponentProps} from "frontend/src/lib/PanelComponentLoader";
+import {CommandBar} from "frontend/src/ctrl/CommandBar";
+import {Datasource} from "frontend/src/lib/Datasource";
+import {LoaderContainer} from "frontend/src/ctrl/LoaderContainer";
+import Icon from "frontend/src/ctrl/Icon";
+import {FilterHelper} from "frontend/src/ctrl/filters/FilterHelper";
+import download from "../../../../../../../../node_modules_shared/frontend/src/lib/Downloader";
+import {FilterPanel} from "../../../../../../../../node_modules_shared/frontend/src/ctrl/filters/FilterPanel";
+
+interface IProps extends IArrowViewComponentProps {
+    groups: any;
+}
+
+export default class ArrowViewComponent extends React.Component<IProps, any> {
+    private columns: Column[];
+    private table: Table;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currEdited: false,
+            dataLoading: false,
+            currEditedData: {},
+            filters: null,
+            filterModalVisible: false,
+        };
+
+
+    }
+
+    _prepareAvaiablePlaces(){
+        let avaiablePlaces = [];
+
+        Object.values(this.props.places).map((el) => {
+            avaiablePlaces.push({
+                key: el,
+                label: el,
+                icon: "ChromeMinimize",
+                onClick: () => {
+                    this.setState({
+                        filters: {place: {
+                                caption: undefined,
+                                condition: "==",
+                                field: "place",
+                                label: el,
+                                labelCaptionSeparator: ":",
+                                value: el,
+                            }}
+                    })
+                }
+            })
+        });
+
+        return avaiablePlaces;
+    }
+
+    public render() {
+        const s = this.state;
+        return (
+            <div>
+                <CommandBar items={[
+                    {key: "al", label: "Dodaj banner", icon: "Add", onClick: () => {
+                            this.props._goto(this.props._baseURL + "/create");
+                        }},
+                ]}/>
+                <Navbar>
+                    <span>{__("Sklep")}</span>
+                    <span>{__("Bannery")}</span>
+
+                    <button
+                        className={"filterBtn"}
+                        onClick={() => {
+                            this.setState({
+                                filterModalVisible: true,
+                            });
+                        }}
+                    >
+                        {__("Filtry")}
+                    </button>
+                </Navbar>
+
+                <div className={"panel-body-margins"}>
+                    <Table remoteURL={this.props._baseURL + `/asyncIndex`}
+                           ref={(el) => this.table = el}
+                           filters={this.state.filters}
+                           rememberState={true}
+                           onFiltersChange={(filters) => this.setState({ filters })}
+                           columns={[
+                               Column.hidden("id"),
+                               Column.text("place", "Miejsce")
+                                   .template((val, row) => {
+                                       return (
+                                           <div>{this.props.places[val]}</div>
+                                       );
+                                   }).className("center").width(180)
+                                   .noFilter()
+                                   .addFilter(FilterHelper.select("place", "Miejsce baneru", this._prepareAvaiablePlaces(), true).get())
+                               ,
+                               Column.bool("active", "Aktywny")
+                                   .onClick((row, val) => {
+                                       const active = row.active;
+                                       Comm._post(this.props._baseURL + `/${row.id}/active`, {active})
+                                           .then(() => {
+                                               this.props._notification("Sukces", "Zmieniono wartość");
+                                               this.table.load();
+                                           })
+                                   }),
+                               Column.text("brand", "Portal").className("center")
+                                   .noFilter().addFilter(FilterHelper.select("brand", "Portal", {
+                                   esotiq: "Esotiq",
+                                   henderson: "Henderson",
+                                   finalsale: "Finalsale",
+                               }, true).get())
+                               ,
+                               Column.text("visibility", "Widoczny").template((val, row) => {
+                                   const rtr = (val == "male") ? "mężczyzna" : (val == "female") ? "kobieta" : "uniwersalny";
+                                   return <div style={{fontSize: "0.8em", color: "#686868", textTransform: "uppercase"}}>{rtr}</div>;
+                               }).className("center")
+                                   .noFilter()
+                                   .addFilter(FilterHelper.select("visibility", "Widoczny", [{value: "male", label: "Mężczyzna"}, {value: "female", label: "Kobieta"}, {value: "all", label: "Uniwersalny"}], true).get())
+                               ,
+                               Column.text("country", "Kraj").template((val) => {
+                                   return <div style={{fontSize: "0.8em", color: "#686868", textTransform: "uppercase"}}>{val}</div>
+                               }),
+                               Column.text("lang", "Język").template((val) => {
+                                   return <div style={{fontSize: "0.8em", color: "#3e3e3e", textTransform: "uppercase"}}>{val}</div>
+                               }),
+                               Column.text("title", "Tytuł"),
+                               Column.text("sort", "Sort").template((val, row) => {
+                                   return (
+                                       <input
+                                           style={{
+                                               padding: "3px 7px",
+                                               width: 50,
+                                               textAlign: "center",
+                                               border: "1px solid lightgrey",
+                                           }}
+                                           type="text"
+                                           defaultValue={val}
+                                           onBlur={(e) => {
+                                               if (e.target.value !== val) {
+                                                   Comm._post(this.props._baseURL + `/sortUpdateManual`, {banner: row.id, sort: e.target.value}).then(() => {
+                                                       this.props._notification("Sukces", "Zmieniono wartość.");
+                                                       this.table.load();
+                                                   });
+                                               }
+                                           }}
+                                       />
+                                   )
+                               }).className("center").width(100),
+                               Column.template("", () => {
+                                   return (
+                                       <Icon name={"ChevronUp"}/>
+                                   )
+                               }).className("center")
+                                   .onClick((row, val) => {
+                                       Comm._get(this.props._baseURL + `/${row.id}/moveUp`)
+                                           .then((response) => {
+                                               this.props._notification("Sukces", "Przeniesiono w górę");
+                                               this.table.load();
+                                           })
+                                   }),
+                               Column.template("", () => {
+                                   return (
+                                       <Icon name={"ChevronDown"}/>
+                                   )
+                               }).className("center")
+                                   .onClick((row, val) => {
+                                       Comm._get(this.props._baseURL + `/${row.id}/moveDown`)
+                                           .then((response) => {
+                                               this.props._notification("Sukces", "Przeniesiono w dół");
+                                               this.table.load();
+                                           })
+                                   }),
+                               Column.template("Edytuj",() => {
+                                   return (
+                                       <Icon name={"edit"}/>
+                                   )
+                               }).className("center")
+                                   .onClick((row, val) => {
+                                   this.props._goto(this.props._baseURL + `/${row.id}/edit`);
+                               }),
+                               Column.template("Usuń",() => {
+                                   return (
+                                       <Icon name={"delete"}/>
+                                   )
+                               }).className("center darkred")
+                                   .onClick((row, val) => {
+                                       Comm._get(this.props._baseURL + `/${row.id}/delete`)
+                                           .then((resp) => {
+                                               this.props._notification("Sukces", "Usunięto pomyślnie");
+                                               this.table.load();
+                                           })
+                                   }),
+                           ]}
+                    />
+                </div>
+
+
+                <Modal
+                    show={this.state.filterModalVisible}
+                    title={__("Filtry")}
+                    animate={true}
+                    animation={"fadeInDown"}
+                    right={25}
+                    top={50}
+                    onHide={() => {
+                        this.setState({filterModalVisible: false});
+                    }}
+                >
+                    <FilterPanel
+                        filters={this.state.filters}
+                        onChange={(filter, filters) => {
+                            this.setState({filters: {...this.state.filters, ...filters}});
+                        }}
+                        items={[
+                            FilterHelper.text("file_name", __("Nazwa pliku"), false),
+                        ]}
+                    />
+                </Modal>
+
+            </div>
+        );
+    }
+}
---
+++ src/packages/cms/views/pagecontroller/index.component.tsx
@@ -0,0 +1,242 @@
+import * as React from "react";
+import Navbar from "frontend/src/ctrl/Navbar";
+import { Column, Table } from "frontend/src/ctrl/Table";
+
+import { confirm, Modal } from "frontend/src/ctrl/Overlays";
+import { BForm, BSelect, BSwitch, BText, BTextarea, BWysiwig } from "frontend/src/layout/BootstrapForm";
+import Comm from "frontend/src/lib/Comm";
+import { Row } from "frontend/src/layout/BootstrapLayout";
+
+import { IArrowViewComponentProps } from "frontend/src/lib/PanelComponentLoader";
+import { CommandBar } from "frontend/src/ctrl/CommandBar";
+import { Datasource } from "frontend/src/lib/Datasource";
+import { LoaderContainer } from "frontend/src/ctrl/LoaderContainer";
+import Icon from "frontend/src/ctrl/Icon";
+import { FilterHelper } from "frontend/src/ctrl/filters/FilterHelper";
+
+interface IProps extends IArrowViewComponentProps {
+    groups: any;
+}
+
+export default class ArrowViewComponent extends React.Component<IProps, any> {
+    private columns: Column[];
+    private table: Table;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currEdited: false,
+            dataLoading: false,
+            currEditedData: {},
+            filters: {
+                brand: {
+                    caption: "Portal",
+                    condition: "LIKE",
+                    field: "brand",
+                    label: "esotiq",
+                    labelCaptionSeparator: "zawiera :",
+                    value: "esotiq"
+                }
+            },
+        };
+
+        this.columns = [
+            Column.text("name", __("Nazwa")).template((val, row) => (
+                <div style={{ marginLeft: 30 * (row.depth - 1) }}>
+                    {row.type == "container" ? <Icon name={"OpenFolderHorizontal"} /> : <Icon name={"FileASPX"} />}{" "}
+                    {val}{" "}
+                </div>
+            )),
+            Column.id("id", "Id").noFilter(),
+            Column.text("country", __("Kraj"))
+                .width(70)
+                .className("center"),
+            Column.text("brand", __("Portal"))
+                .width(70)
+                .className("center")
+                .noFilter().addFilter(FilterHelper.select("brand", "Portal", {
+                esotiq: "Esotiq",
+                henderson: "Henderson",
+                finalsale: "Finalsale",
+            }, true).get())
+            ,
+            Column.bool("active", __("Aktywna")),
+            Column.text("type", __("Typ"))
+                .noFilter()
+                .addFilter(
+                    FilterHelper.select(
+                        "type",
+                        __("Typ"),
+                        [
+                            { value: "page", label: "page" },
+                            { value: "folder", label: "folder" },
+                            { value: "link", label: "link" }
+                        ],
+                        false
+                    ).get()
+                ),
+            Column.text("link", __("Link")),
+
+            Column.template("", () => {
+                return <Icon name={"OpenInNewWindow"} />;
+            })
+                .className("center")
+                .onClick((row) => {
+                    window.open(`https://www.esotiq.com/${this.props.language}/${this.props.language}/${row.link}`);
+                }),
+
+            props.editEnabled
+                ? Column.template("", () => <Icon name={"ChevronDown"} />)
+                      .className("center")
+                      .onClick((row) => {
+                          Comm._post(this.props._baseURL + "/moveDown", { key: row.id }).then((result) => {
+                              this.table.load();
+                          });
+                      })
+                : null,
+            props.editEnabled
+                ? Column.template("", () => <Icon name={"ChevronUp"} />)
+                      .className("center")
+                      .onClick((row) => {
+                          Comm._post(this.props._baseURL + "/moveUp", { key: row.id }).then((result) => {
+                              this.table.load();
+                          });
+                      })
+                : null,
+
+            Column.template("", () => <Icon name={"Edit"} />)
+                .className("center")
+                .onClick((row) => {
+                    this.props._goto(this.props._baseURL + "/edit", { key: row.id });
+                }),
+
+            props.editEnabled
+                ? Column.template("", () => <Icon name={"Delete"} />)
+                      .className("center darkred")
+                      .onClick((row) => this.handleDelete(row))
+                : null,
+            Column.hidden("depth")
+        ];
+    }
+
+    public handleDelete(row) {
+        confirm(__("Czy na pewno usunąć") + ` "${row.name}"?`).then(() => {
+            Comm._post(this.props._baseURL + "/delete", { key: row.id }).then(() => {
+                this.props._notification(__("Pomyślnie usunięto") + ` "${row.name}"`);
+                this.table.load();
+            });
+        });
+    }
+
+    public loadObjectData() {
+        if (this.state.currEdited != -1) {
+            this.setState({ loading: true });
+            Comm._post(this.props._baseURL + "/get", { key: this.state.currEdited }).then((response) =>
+                this.setState({ currEditedData: response, loading: false })
+            );
+        } else {
+            this.setState({ currEditedData: {} });
+        }
+    }
+
+    public render() {
+        const s = this.state;
+
+        const brandFilter = {
+            brand: {
+                caption: "Portal",
+                condition: "LIKE",
+                field: "brand",
+                label: "finalsale",
+                labelCaptionSeparator: "zawiera :",
+                value: "finalsale"
+            }
+        }
+        console.log(this.state.filters, "FILTREY");
+
+        return (
+            <div>
+                <CommandBar
+                    isSearchBoxVisible={false}
+                    onSearch={(search) => alert("To implement " + search)}
+                    items={[
+                        {
+                            key: "f1",
+                            label: __("Dodaj") + "",
+                            icon: "Add",
+                            onClick: () => this.setState({ currEdited: -1 })
+                        }
+                    ]}
+                />
+                <Navbar>
+                    <span>{__("CMS")}</span>
+                    <span>{__("Strony wwww")}</span>
+                </Navbar>
+                <div style={{ padding: "0 10px" }}>
+                    <Table
+                        columns={this.columns}
+                        remoteURL={this.props._baseURL + "/asyncIndex"}
+                        ref={(table) => (this.table = table)}
+                        filters={this.state.filters}
+                        rememberState={true}
+                        onFiltersChange={(filters) => {
+                            this.setState({ filters })
+                        }}
+                    />
+                </div>
+
+                <Modal
+                    title={(s.currEdited == -1 ? __("Dodanie") : __("Edycja")) + __(" strony www")}
+                    show={s.currEdited != false}
+                    onHide={() => this.setState({ currEdited: false })}
+                    showHideLink={true}
+                >
+                    <BForm
+                        data={{
+                            type: "page"
+                        }}
+                        action={this.props._baseURL + "/add"}
+                        namespace={"data"}
+                        onSuccess={(e) => {
+                            this.props._notification(this.state.currEditedData.name, __("Zapisano pomyślnie"));
+                            this.setState({ currEdited: -1 });
+                            this.table.load();
+                        }}
+                    >
+                        {(form) => (
+                            <div style={{ padding: 10, width: 400 }} className="">
+                                <Row noGutters={false}>
+                                    <BText label={__("Nazwa")} {...form("name")} />
+                                    <BSwitch
+                                        label={__("Typ")}
+                                        options={[
+                                            { label: "Strona", value: "page" },
+                                            { label: "Folder", value: "container" }
+                                        ]}
+                                        {...form("type", "page")}
+                                    />
+                                </Row>
+                                <Row noGutters={false}>
+                                    <BSelect
+                                        label={__("Element nadrzędny")}
+                                        options={this.props.containers.map((el) => ({value: el.id, label: el.name}))}
+                                        {...form("parent_id")}
+                                    />
+                                </Row>
+
+                                <div className="hr-line-dashed" />
+                                <button
+                                    onClick={() => this.setState({ currEdited: false })}
+                                    className="btn btn-default pull-right"
+                                >
+                                    {__("Anuluj")}
+                                </button>
+                                <button className="btn btn-primary pull-right">{__("Zapisz")}</button>
+                            </div>
+                        )}
+                    </BForm>
+                </Modal>
+            </div>
+        );
+    }
+}
