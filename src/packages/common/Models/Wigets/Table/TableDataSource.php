1ed5181c8d08e71f029383be545f2a9d88bce706
Artur Kmera <artur.kmera@as-pl.com>
2020-02-27 17:50:12 +0100

    class to separate file

--- src/packages/common/Models/Wigets/Table/TableDataSource.php
+++ src/packages/common/Models/Wigets/Table/TableDataSource.php
@@ -15,31 +15,6 @@ use Arrow\ORM\Persistent\Criteria;
 use Arrow\ORM\Persistent\DataSet;
 
 
-interface ITableDataSource
-{
-    const FILTER_EQUAL = "==";
-    const FILTER_IN = "IN";
-    const FILTER_NOT_IN = "NOT IN";
-    const FILTER_LIKE = "LIKE";
-    const FILTER_NOT_EQUAL = "!=";
-    const FILTER_NOT_LIKE = "NOT LIKE";
-    const FILTER_START_WITH = "^%";
-    const FILTER_END_WITH = "%$";
-
-    public function getRows($columns, $debug);
-
-    public function addOrder($order, $direction);
-
-    public function limit($index, $length);
-
-    public function count();
-
-    public function applyFilterSearch($field, $value, $type, $filter);
-
-    public function dataPrepared($data);
-
-}
-
 
 class TableDataSource extends Criteria
 {
