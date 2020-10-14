7752424eb73cd7759f0d7b03dce34ba0f3a3c690
Artur Kmera <artur.kmera@as-pl.com>
2020-10-14 08:57:56 +0200

    Error display disabled in production

--- src/server/models/exception/ExceptionHandler.php
+++ src/server/models/exception/ExceptionHandler.php
@@ -88,7 +88,7 @@ class ExceptionHandler
             ob_start("ob_gzhandler");
         }
 
-        if ($_ENV["APP_ENV"] !== "dev" && false) {
+        if ($_ENV["APP_ENV"] !== "dev" ) {
             print $this->printPublicMinimumMessage($hash);
         } else {
             $request = Kernel::getProject()
