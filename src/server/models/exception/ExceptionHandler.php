93f18594a8a21be1801e8c9658335af8244895c7
Artur Kmera <artur.kmera@3code.pl>
2021-05-30 09:25:32 +0200

    Env error update

--- src/server/models/exception/ExceptionHandler.php
+++ src/server/models/exception/ExceptionHandler.php
@@ -115,7 +115,7 @@ class ExceptionHandler
             ob_start("ob_gzhandler");
         }
 
-        if (  false && !isset($_ENV["APP_ENV"]) || $_ENV["APP_ENV"] !== "dev"  ) {
+        if (  false && !isset($_ENV["APP_ENV"]) || isset($_ENV["APP_ENV"])  && $_ENV["APP_ENV"] !== "dev"  ) {
             print $this->printPublicMinimumMessage($hash);
         } else {
             $request = Kernel::getProject()
