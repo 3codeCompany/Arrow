9360d588526dc08483dfc40cfab1f0237ef82b51
Artur Kmera <artur.kmera@as-pl.com>
2021-08-18 14:53:48 +0200

    Added icons to dictionary

--- src/packages/common/views/dictionarycontroller/index.component.tsx
+++ src/packages/common/views/dictionarycontroller/index.component.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 
-import {Navbar} from "serenity-controls/lib/Navbar";
-import {Row} from "serenity-controls/lib/Row";
-import {IArrowViewComponentProps} from "serenity-controls/lib/backoffice";
-import {BForm, BText, BTextarea} from "serenity-controls/lib/BForm";
+import { Navbar } from "serenity-controls/lib/Navbar";
+import { Row } from "serenity-controls/lib/Row";
+import { IArrowViewComponentProps } from "serenity-controls/lib/backoffice";
+import { BForm, BText, BTextarea } from "serenity-controls/lib/BForm";
 
-import {Column, Table} from "serenity-controls/lib/Table";
-import {CommandBar} from "serenity-controls/lib/CommandBar";
-import {Icon} from "serenity-controls/lib/Icon";
-import {Comm} from "serenity-controls/lib/lib";
-import {Modal} from "serenity-controls/lib/Modal";
-import {confirmDialog} from "serenity-controls/lib/ConfirmDialog";
+import { Column, Table } from "serenity-controls/lib/Table";
+import { CommandBar } from "serenity-controls/lib/CommandBar";
+
+import { Comm } from "serenity-controls/lib/lib";
+import { Modal } from "serenity-controls/lib/Modal";
+import { confirmDialog } from "serenity-controls/lib/ConfirmDialog";
+import { CommonIcons } from "serenity-controls/lib/lib/CommonIcons";
 
 interface IComponentProps extends IArrowViewComponentProps {
     parent: any;
@@ -29,7 +30,7 @@ export default class ArrowViewComponent extends React.Component<IComponentProps,
         super(props);
         this.state = {
             add: false,
-            edited: null
+            edited: null,
         };
     }
 
@@ -53,24 +54,26 @@ export default class ArrowViewComponent extends React.Component<IComponentProps,
                         {
                             key: "f1",
                             label: "Dodaj",
-                            icon: "Add",
-                            onClick: () => this.setState({add: true})
-                        }
+                            icon: CommonIcons.add,
+                            onClick: () => this.setState({ add: true }),
+                        },
                     ]}
                 />
                 <Navbar>
                     <span>System</span>
                     <a onClick={() => this.props._goto(this.props._baseURL + "/list/1")}>Słowniki</a>
                     {this.props.ancestors.length > 1
-                        ? this.props.ancestors.filter((el) => el.id != 1).map((el) => (
-                            <a
-                                onClick={() => {
-                                    this.props._goto(this.props._baseURL + "/list/" + el.id);
-                                }}
-                            >
-                                {el.label}
-                            </a>
-                        ))
+                        ? this.props.ancestors
+                              .filter((el) => el.id != 1)
+                              .map((el) => (
+                                  <a
+                                      onClick={() => {
+                                          this.props._goto(this.props._baseURL + "/list/" + el.id);
+                                      }}
+                                  >
+                                      {el.label}
+                                  </a>
+                              ))
                         : null}
                     {this.props.parent.id != 1 && <span>{this.props.parent.label}</span>}
                 </Navbar>
@@ -89,14 +92,12 @@ export default class ArrowViewComponent extends React.Component<IComponentProps,
                                 .template((val, row) => {
                                     return (
                                         <>
-                                            <div className="pull-right">
-                                                icon
-                                            </div>
+                                            <div className="pull-right"></div>
                                             {row.system_name ? (
                                                 <span>
                                                     {row.label}
-                                                    <br/>
-                                                    <small style={{color: "darkgrey"}}>{row.system_name}</small>
+                                                    <br />
+                                                    <small style={{ color: "darkgrey" }}>{row.system_name}</small>
                                                 </span>
                                             ) : (
                                                 val
@@ -120,52 +121,41 @@ export default class ArrowViewComponent extends React.Component<IComponentProps,
                                 }),
 
                             Column.text("value", "Wartość"),
-                            Column.text("data", "Dane").template((val) => <pre style={{whiteSpace: "pre-wrap"}}>{val}</pre>),
+                            Column.text("data", "Dane").template((val) => <pre style={{ whiteSpace: "pre-wrap" }}>{val}</pre>),
                             Column.template("", () => <CommonIcons.edit />)
                                 .className("center")
                                 .width(40)
-                                .onClick((row) => this.setState({add: true, edited: row})),
+                                .onClick((row) => this.setState({ add: true, edited: row })),
                             Column.template("", () => <CommonIcons.delete />)
                                 .className("center darkred")
                                 .width(40)
-                                .onClick(this.handleDelete)
+                                .onClick(this.handleDelete),
                         ]}
                     />
 
-                    <Row/>
+                    <Row />
                 </div>
 
-                <Modal
-                    show={s.add}
-                    title={"Dodaj wartość do " + p.parent.label}
-                    showHideLink={true}
-                    onHide={() => this.setState({add: false, edited: null})}
-                >
-                    <div style={{padding: 10, width: 500}}>
+                <Modal show={s.add} title={"Dodaj wartość do " + p.parent.label} showHideLink={true} onHide={() => this.setState({ add: false, edited: null })}>
+                    <div style={{ padding: 10, width: 500 }}>
                         <BForm
                             action={this.props._baseURL + (s.edited !== null ? "/save/" : "/create/") + p.parent.id}
                             namespace={"data"}
-                            data={this.state.edited ? {...this.state.edited} : {}}
+                            data={this.state.edited ? { ...this.state.edited } : {}}
                             onSuccess={() => {
                                 this.props._notification(s.edited ? "Zapisano zmiany" : "Dodano");
-                                this.setState({add: false, edited: null});
+                                this.setState({ add: false, edited: null });
                                 this.list.load();
                             }}
                         >
                             {(form) => {
                                 return (
                                     <>
-                                        <BText label={"Etykieta"} {...form("label")} autoFocus={true}/>
+                                        <BText label={"Etykieta"} {...form("label")} autoFocus={true} />
                                         <BText label={"Wartosc"} {...form("value")} />
-                                        <BTextarea label={"Dane dodatkowe"} {...form("data")} style={{height: 100}}/>
-                                        <BText
-                                            label={"Nazwa systemowa"}
-                                            {...form("system_name")}
-                                            help={"Modyfikuj tylko jeśli wiesz czo robisz!"}
-                                        />
-                                        <button className="pull-right btn btn-primary">
-                                            {s.edited ? "Zmień" : "Dodaj"}
-                                        </button>
+                                        <BTextarea label={"Dane dodatkowe"} {...form("data")} style={{ height: 100 }} />
+                                        <BText label={"Nazwa systemowa"} {...form("system_name")} help={"Modyfikuj tylko jeśli wiesz czo robisz!"} />
+                                        <button className="pull-right btn btn-primary">{s.edited ? "Zmień" : "Dodaj"}</button>
                                     </>
                                 );
                             }}
